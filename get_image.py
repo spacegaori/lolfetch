fe13c6c4e862cc06d17bdbc81c077f378499b8cf
Soomin Choi <spacegaori@gmail.com>
2022-10-04 00:39:46 -0400

    file clean up

--- get_image.py
+++
@@ -1,6 +0,0 @@
-import urllib.request
-
-def get_image(champion_name):
-    url = 'https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/' + champion_name + '.png'
-    dir = champion_name + '.png'
-    urllib.request.urlretrieve(url, dir)
