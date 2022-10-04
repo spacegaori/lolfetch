5c34ba52985a8b68c4803b79afe2b9f03f94f443
Soomin Choi <spacegaori@gmail.com>
2022-10-04 00:37:43 -0400

    Modularized code

---
+++ helper_functions.py
@@ -0,0 +1,101 @@
+from riotwatcher import LolWatcher
+import argparse
+
+lol_watcher = LolWatcher(' ')
+
+def get_champion_name():
+    parser = argparse.ArgumentParser(description = 'Display champion information.')
+    parser.add_argument('champion_name', type = str, help = 'Name of champion')
+    arguments = parser.parse_args()
+
+    return arguments.champion_name.capitalize()
+    
+def validate_arguments(argument_champion_name, champion_names):
+    return argument_champion_name in champion_names
+
+def get_latest_version(region = 'na1'): # default region is North America
+    region = region                     # other regions are available as well
+    version = lol_watcher.data_dragon.versions_for_region(region)
+    
+    return version
+
+def get_champion_data():
+    version = get_latest_version()
+    champion_version = version['n']['champion']
+    champion_data = lol_watcher.data_dragon.champions(champion_version)['data']
+    
+    return champion_data
+
+def get_champion_names():
+    champion_data = get_champion_data()
+    champion_names = list()
+
+    for champion in champion_data:
+        champion_names.append(champion)
+    
+    return champion_names
+
+def get_data(champion_name):
+    return get_champion_data()[champion_name]
+
+def get_champion_title(champion_name):
+    return (get_data(champion_name))['title']
+
+def get_champion_blurb(champion_name):
+    return (get_data(champion_name))['blurb']
+
+def get_champion_info(champion_name):
+    return (get_data(champion_name))['info']
+
+def get_champion_tags(champion_name):
+    return (get_data(champion_name))['tags']
+
+def print_champion_info(champion_name):
+    title = get_champion_title(champion_name)
+    blurb = get_champion_blurb(champion_name)
+    info = get_champion_info(champion_name)
+    tags = get_champion_tags(champion_name)
+    print(champion_name + ', ' + title)
+    print(blurb)
+    for i in info:
+        print(i.capitalize() + ': ' + str(info[i]))
+    for tag in tags:
+        print(tag, end='')
+        print(', ', end='')
+
+import urllib.request
+
+def get_champion_image(champion_name):
+    url = 'https://ddragon.leagueoflegends.com/cdn/12.18.1/img/champion/' + champion_name + '.png'
+    directory = champion_name + '.png'
+    urllib.request.urlretrieve(url, directory)
+
+
+from colors import color
+from PIL import Image
+import numpy as np
+
+def get_image_pixel(col):
+    return color('  ', bg=f'rgb({int(col[0])}, {int(col[1])}, {int(col[2])})')
+
+def render_image(pixels, scale = 20):
+    image_size = pixels.shape[:2]
+    block_size = (image_size[0] / scale[0], image_size[1] / scale[1])
+    blocks = []
+    y = 0
+    while y < image_size[0]:
+        x = 0
+        block_col = []
+        while x < image_size[1]:
+            block_col.append(pixels[int(y):int(y+block_size[0]), int(x):int(x+block_size[1])].reshape(-1, 3).mean(axis=0))
+            x += block_size[1]
+        blocks.append(block_col)
+        y += block_size[0]
+    output = [[get_image_pixel(block) for block in row] for row in blocks]
+    
+    return output
+
+def print_image(champion_name):
+    image = np.asarray(Image.open(champion_name + '.png'))
+    output = render_image(image, (20, 20))
+    print('\n'.join([''.join(row) for row in output]))
--- lolfetch.py
+++ lolfetch.py
@@ -1,30 +1,88 @@
-from riotwatcher import LolWatcher, ApiError
-from imgrender import render
-from PIL import Image
-import get_image
+# from colors import color
+# from PIL import Image, ImageFilter
+# import numpy as np
+# from riotwatcher import LolWatcher, ApiError
+# import lolfetch.get_champion_icon as get_champion_icon
 import os
 
-
-lol_watcher = LolWatcher('RGAPI-d79c0d9c-1e9c-4a84-abb8-1aff12c203cd')
-
-my_region = 'na1'
-
-versions = lol_watcher.data_dragon.versions_for_region(my_region)
-champions_version= versions['n']['champion']
-current_champ_list = lol_watcher.data_dragon.champions(champions_version)['data']
-
-champion_name = input('Name of the champion?\n')
-get_image.get_image(champion_name)
-                      
-champion = current_champ_list[champion_name]
-name = champion['name']
-title = champion['title']
-blurb = champion['blurb']
-
-print(name + ", " + title + "\n" + blurb)
-
-render(champion_name + '.png', scale = (25, 25))
-
-os.remove(champion_name + '.png') 
+import helper_functions
 
 
+# def get_pixel(col):
+#     return color('  ', bg=f'rgb({int(col[0])}, {int(col[1])}, {int(col[2])})')
+
+# def render_image(pixels, scale):
+#     # first of all scale the image to the scale 'tuple'
+#     image_size = pixels.shape[:2]
+#     block_size = (image_size[0]/scale[0], image_size[1]/scale[1])
+#     blocks = []
+#     y = 0
+#     while y < image_size[0]:
+#         x = 0
+#         block_col = []
+#         while x < image_size[1]:
+#             # get a block, reshape in into an Nx3 matrix and then get average of each column
+#             block_col.append(pixels[int(y):int(y+block_size[0]), int(x):int(x+block_size[1])].reshape(-1, 3).mean(axis=0))
+#             x += block_size[1]
+#         blocks.append(block_col)
+#         y += block_size[0]
+#     output = [[get_pixel(block) for block in row] for row in blocks]
+#     return output
+
+# def find_dominant_color(filename):
+#     #Resizing parameters
+#     width, height = 150,150
+#     image = Image.open(filename)
+#     image = image.resize((width, height),resample = 0)
+#     #Get colors from image object
+#     pixels = image.getcolors(width * height)
+#     #Sort them by count number(first element of tuple)
+#     sorted_pixels = sorted(pixels, key=lambda t: t[0])
+#     #Get the most frequent color
+#     dominant_color = sorted_pixels[-1][1]
+#     return dominant_color
+
+champion_name = helper_functions.get_champion_name()
+
+# version = helper_functions.get_latest_version()
+# champions = helper_functions.get_champion_data()
+champion_names = helper_functions.get_champion_names()
+
+if(not helper_functions.validate_arguments(champion_name, champion_names)):
+    print('Incorrect argument.')
+    quit()
+
+helper_functions.get_champion_image(champion_name)
+helper_functions.print_image(champion_name)
+helper_functions.print_champion_info(champion_name)
+
+# champion_name = input('Name of the champion?\n')
+# get_champion_icon.get_image(champion_name)
+# get_champion          
+# champion = current_champ_list[champion_name]
+
+# version = champion['version']
+# id = champion['id']
+# key = champion['key']
+# name = champion['name']
+# title = champion['title']
+# blurb = champion['blurb']
+# info = champion['info']
+# image = champion['image']
+# tags = champion['tags']
+# partype = champion['partype']
+# stats = champion['stats']
+
+# for champ in current_champ_list:
+#     print(champ)
+
+# # print(version + "\n" + id + "\n" + key + "\n" + name + ", " + title + "\n" + blurb + "\n" + info + "\n" + image + "\n" + tags + "\n" + partype + "\n" + stats + "\n")
+# print(name + ", " + title + "\n" + blurb + "\n" + stats + "\n")
+
+# domcol = find_dominant_color(champion_name + '.png')
+# print(domcol)
+# image = np.asarray(Image.open(champion_name + '.png'))
+# output = render_image(image, (20, 20))
+# print('\n'.join([''.join(row) for row in output]))
+
+# os.remove(champion_name + '.png') 
